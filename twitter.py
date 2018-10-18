daf5ec5e2bf7e9124677ef37189e1790a4bdc3e9
Pulkit gera <pulkithifi@yahoo.com>
2018-10-18 17:31:57 +0530

    twiiter.py file

--- twitter.py
+++ twitter.py
@@ -9,21 +9,50 @@ auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
 user = api.me()
-""" search = "ecell_iiithyd"
 number = "50"
 
-for tweet in tweepy.Cursor(api.search, search).items(number):
-    try:
-        tweet.retweet()
-        print('retweeted') 
-        print(tweet.text)
+""" This function retweets for a given keyword """
+def retweet(keyword):
+    for tweet in tweepy.Cursor(api.search, keyword).items(number):
+        try:
+            tweet.retweet()
+            print('retweeted') 
+            print(tweet.text)
 
-    except tweepy.TweepError as e:
-        print(e.reason)
-    except StopIteration:
-        break
-    except KeyboardInterrupt:
-        print('Tweeting done for the day')
-        break """
+        except tweepy.TweepError as e:
+            print(str(e.message[0]['message']))
+        except StopIteration:
+            break
+        except KeyboardInterrupt:
+            print('Tweeting done for the day')
+            break
 
-print(user)
+
+""" This function returns data about a tweet such as text and basic info about that person """
+def searching(keyword):
+    for tweet in tweepy.Cursor(api.search, keyword).items(1):
+        try:
+            texty =  tweet.text
+            author = tweet.author
+            name = author.name
+            screen_name = author.screen_name
+            followers = author.followers_count
+            print("Name: "+ name)
+            print("Screen Name : " + screen_name)
+            print("Followers : " + str(followers))
+            print("Text : " +texty)
+            print("Do we follow : " + str(author.following))
+            
+        except tweepy.TweepError as e:
+            print(str(e.message[0]['message']))
+        except StopIteration:
+            break
+        except KeyboardInterrupt:
+            print('Searched done for the day')
+            break
+
+if __name__ == '__main__':
+    keyword =  raw_input("Enter keyword for retweet ")
+    # retweet(keyword)
+    searching(keyword)
+
